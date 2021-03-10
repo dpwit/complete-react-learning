a40524fcd6f918e526e6b98a479ddb6df2c7034a
dpwit <darren.whatford@dpwit.co.uk>
2021-03-10 21:45:20 +0000

    Updated Link.js from ES5 to ES6 code

--- src/Link.js
+++ src/Link.js
@@ -1,6 +1,14 @@
 import React from 'react'
 
-function Link({ href='', target, children }) {
+const Link = props => {
+
+	const {
+		href = '',
+		rel,
+		target,
+		children
+	} = props
+
 	return (
 		<a href={ href } target={ target } className='link'>{ children }</a>
 	)
