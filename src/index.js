e9f63018612ce70ed73f1e4608189be8d77b1209
darrenwhatford <darrenwhatford@hastingsdirect.com>
2021-03-10 17:00:51 +0000

    Added props and welcome and link component

--- src/index.js
+++ src/index.js
@@ -1,9 +1,36 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import {render} from 'react-dom';
 
-	ReactDOM.render(
-		<h1>Hello World</h1>,
-		document.getElementById('app')
-		)
+function Link({ href='', target, children }) {
+	return (
+		<a href={ href } target={ target } className='link'>{ children }</a>
+	)
+}
 
-		
+function Welcome(props) {
+	return (
+		<h1 className='heading'>Hello {props.name} !</h1>
+	)
+}
+
+function App() {
+	return (
+		<>
+			<Welcome name="Darren" /> 
+			<Link href="https://www.google.com" target="_blank">Google</Link>
+			<Welcome name="Dan" />
+			<Link href="https://www.bbc.com">BBC</Link>
+			<Welcome name="Dave" />
+			<Link>YouTube</Link>
+			<Welcome name="Scott" />
+			<Link>Bing</Link>
+		</>
+	)
+}
+
+render(
+	<App />,
+	document.getElementById('app')
+)
+
+
