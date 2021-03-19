8a0bf6c1d6d47a8bb2b89870b2334a9ef82b1e32
darrenwhatford <darrenwhatford@hastingsdirect.com>
2021-03-19 16:40:42 +0000

    Added a new link

--- src/components/Templates/About.js
+++ src/components/Templates/About.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const About = () => {
 
     return (
-        <h1>About</h1>
+        <>
+            <h1>About</h1>
+            <Link to='/'>Home</Link>
+        </>
     )
 }
 
